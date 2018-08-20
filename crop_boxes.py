b23e4dc3782133e7017ffdcadc2a9c70e3c5501f
lemonci <hongyanchangfei@gmail.com>
2018-08-20 11:17:07 +0900

    Update crop_boxes.py

--- crop_boxes.py
+++ crop_boxes.py
@@ -1,11 +1,13 @@
+
 from PIL import Image
-filename = [f for f in os.listdir("filepath") if f.endswith('.png')]
-print(filename)
+
+os.chdir(“<image path>")
+filename = [f for f in os.listdir("<image path>") if f.endswith('.png')]
 for a in filename:
     img = Image.open(a)
-    for hc in list(range(0, 9)):
+    for hc in list(range(0, 10)):
         hs = 16 + hc * 69
-        for wc in list(range(0, 8)):
+        for wc in list(range(0, 9)):
             ws = 16 + wc * 69
             #(85, 16, 152, 83) == chu
             img.crop((ws, hs, ws + 67, hs + 67)).save(str(wc)+str(hc)+"_"+ a )
